50789003bb24bf8622b54c272d389d24f4969b91
debiprasadsahoo16 <99535126+debiprasadsahoo16@users.noreply.github.com>
2022-03-07 11:28:13 +0530

    Add files via upload

---
+++ W2_Q1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+    char s1[30],s2[30];
+    int l,i;
+    gets(s1);
+    gets(s2);
+    strcat(s1,s2);
+    l=strlen(s1);
+    for(i=l-1;i>=0;i--)
+    {
+        printf("%c",s1[i]);
+    }
+    return 0;
+}
